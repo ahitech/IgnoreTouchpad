5fdb927aab556e39ad992e3fb863408359fde874
Alex Hitech <ahitech@gmail.com>
2025-08-22 19:32:59 +0300

    Usage fix

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -73,21 +73,17 @@ void PrintUsage() {
 	printf(B_TRANSLATE("This utility disables or enables a pointing device (mouse or touchpad). "
 		   "Its aim is to ignore accidental clicks on the touchpad when an external pointing "
 		   "device is connected to a notebook.\n"));
-	printf(B_TRANSLATE("\nIt can run in either interactive or non-interactive mode.\n"));
-	printf(B_TRANSLATE( "\tNOTE: Only interactive mode is atomic!\n"
-						"\tIf you connect or disconnect a device between listing them in non-\n"
-						"\tinteractive mode and sending a command to enable or disable a device,\n"
-						"\tyou may end up disabling or enabling a wrong device!\n"));
+	printf(B_TRANSLATE("It can run in either interactive or non-interactive mode.\n\n"));
 	printf(B_TRANSLATE("Supported options (in both modes, unless stated othwerwise):\n"));
-	printf(B_TRANSLATE("  list       - Print a numbered list of the pointing input devices.\n"));
-	printf(B_TRANSLATE("  enable #   - Enable a device number #. The number you take from the \"list\" command.\n"));
-	printf(B_TRANSLATE("               If a device is already enabled, or if the number is wrong, nothing happens.\n"));
-	printf(B_TRANSLATE("  disable #  - Disable a device number #. The number you take from the \"list\" command.\n"));
-	printf(B_TRANSLATE("               If a device is already disabled, or if the number is wrong, nothing happens.\n"));
-	printf(B_TRANSLATE("  enable_all - Immediately enable all devices. If you accidentally disabled the last\n"
-					   "               mouse, you can enable it.\nDefault shortcut: Ctrl + Alt + Win + E.\n"
-					   "               (You can change in \'Shortcuts\', if you want, but this text won't be updated.\n"));
-	printf(B_TRANSLATE("  help       - Display list of the available commands.\n"));
+	printf(B_TRANSLATE("  list        - Print a numbered list of the pointing input devices.\n"));
+	printf(B_TRANSLATE("  enable #    - Enable a device number #. The number you take from the \"list\" command.\n"));
+	printf(B_TRANSLATE("                If a device is already enabled, or if the number is wrong, nothing happens.\n"));
+	printf(B_TRANSLATE("  disable #   - Disable a device number #. The number you take from the \"list\" command.\n"));
+	printf(B_TRANSLATE("                If a device is already disabled, or if the number is wrong, nothing happens.\n"));
+	printf(B_TRANSLATE("  enable_all  - Immediately enable all devices. If you accidentally disabled the last\n"
+					   "                mouse, you can enable it.\nDefault shortcut: Ctrl + Alt + Win + E.\n"
+					   "                (You can change in \'Shortcuts\', if you want, but this text won't be updated.\n"));
+	printf(B_TRANSLATE("  help        - Display list of the available commands.\n"));
 	printf(B_TRANSLATE("  interactive - (Command line option only) Enter interactive mode.\n"));
-	printf(B_TRANSLATE("  quit       - (Interactive mode only) Quit interactive mode.\n"));
+	printf(B_TRANSLATE("  quit        - (Interactive mode only) Quit interactive mode.\n"));
 }
